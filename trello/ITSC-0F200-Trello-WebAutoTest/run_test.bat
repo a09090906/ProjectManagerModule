721b14fd1872b085908cb8348c7022e4ff8bd9a2
Fanny <fannyapriana@gmail.com>
2022-09-29 10:18:00 +0800

    remove 2

--- trello/ITSC-0F200-Trello-WebAutoTest/run_test.bat
+++
@@ -1,7 +0,0 @@
-echo off
-SET batchpath=%~dp0
-del /Q "%batchpath%\log\*.html"
-del /Q "%batchpath%\log\*.png"
-del /Q "%batchpath%\log\*.xml"
-del /Q "%batchpath%\temp\*.csv"
-robot --outputdir "%batchpath%\log" --exclude NA --exclude Title --variable "gTrello URL":%*
